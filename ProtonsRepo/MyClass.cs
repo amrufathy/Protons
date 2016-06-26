b874cb6a77d7797f5f4257506dca0a972d998d0b
Ahmed Hamdy <ahmedhamdyau@hotmail.com>
2016-06-26 08:34:18 +0200

    Added class syntax and the stack syntax

---
+++ ProtonsRepo/MyClass.cs
@@ -0,0 +1,32 @@
+﻿namespace StackSession
+{
+    public class MyClass
+    {
+        private string myField;
+
+        private int operationCount; // Tells the number of changes done to the property
+        private int myIntPropertyBackingField;
+
+        public int MyIntProperty
+        {
+            get { return myIntPropertyBackingField; }
+            set
+            {
+                // Increment the number of operations done to this property
+                operationCount++;
+
+                // Set the value of the property
+                myIntPropertyBackingField = value;
+            }
+        }
+
+        public int OperationCount
+        {
+            // This property is read only, no one from outside the class can change its value
+            get { return myIntPropertyBackingField; }
+        }
+
+        public string MyAutomaticProperty { get; set; }
+
+    }
+}
