67352ff1edd0ab0706772b11853188efea13f4b0
Ahmed Hamdy <ahmedhamdyau@hotmail.com>
2016-06-26 08:26:11 +0200

    Add project files.

---
+++ ProtonsRepo/MyStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackSession
+{
+    public class MyStack
+    {
+        private List<int> data = new List<int>();
+
+        public void Push(int item)
+        {
+            data.Add(item);
+        }
+
+        public int Pop()
+        {
+            // If the stack is empty, throw an exception ( error )
+            if (data.Count == 0) throw new InvalidOperationException("Stack is empty");
+
+            // Get the last element in the list
+            int lastElementIndex = data.Count - 1;
+            int retVal = data[lastElementIndex];
+
+            // Remove the item from the stack
+            data.RemoveAt(lastElementIndex);
+
+            return retVal;
+        }
+
+        public int Count()
+        {
+            return data.Count;
+        }
+    }
+}
---
+++ ProtonsRepo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StackSession
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MyStack stack = new MyStack();
+
+            // Add 10 numbers to the stack
+            int numCount = 10;
+            for (int i = 0; i < numCount; i++)
+            {
+                stack.Push(i);
+                Console.WriteLine("Pushed:" + i);
+            }
+
+            Console.WriteLine(".....................");
+
+            // Retrieve the 10 numbers ( will be in reversed order )
+            int bound = stack.Count();
+
+            for (int i = 0; i < bound; i++)
+            {
+                Console.WriteLine("Popped:" + stack.Pop());
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
