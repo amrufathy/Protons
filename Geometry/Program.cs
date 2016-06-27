19206336161f1129eded68a57cb6ea9f738105f3
Ahmed Hamdy <ahmedhamdyau@hotmail.com>
2016-06-27 18:01:39 +0200

    Added the geometry project

---
+++ Geometry/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Geometry
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ProtonsPoint p = ReadPoint();
+            ProtonsPoint otherP = ReadPoint();
+
+            Console.WriteLine(p.Distance(otherP));
+
+            p.MoveTo(5,78);
+            Console.WriteLine(p.TransformToString());
+
+            Console.ReadKey();
+        }
+
+        private static ProtonsPoint ReadPoint()
+        {
+            Console.WriteLine("Enter the X coordinate of the point");
+            string userInputX = Console.ReadLine();
+            int xCoordinate = int.Parse(userInputX);
+
+            Console.WriteLine("Enter the Y coordinate of the point");
+            string userInputY = Console.ReadLine();
+            int yCoordinate = int.Parse(userInputY);
+
+            return new ProtonsPoint(xCoordinate, yCoordinate);
+        }
+    }
+}
---
+++ Geometry/ProtonsPoint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Geometry
+{
+    internal class ProtonsPoint
+    {
+        private double x;
+        private double y;
+
+        //ProtonsPoint p = new ProtonsPoint(x,y)
+        //Puts x in X
+        //Puts y in Y
+        public ProtonsPoint(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double Distance(ProtonsPoint anotherP)
+        {
+            return Math.Sqrt(Math.Pow(this.x - anotherP.x, 2)
+                + Math.Pow(this.y - anotherP.y, 2));
+        }
+
+        public double Slope(ProtonsPoint anotherP)
+        {
+            return (this.y - anotherP.y) / (this.x - anotherP.x);
+        }
+
+        public void MoveTo(double newX, double newY)
+        {
+            this.x = newX;
+            this.y = newY;
+        }
+
+        public string TransformToString()
+        {
+            return $"({x},{y})";
+        }
+    }
+}
